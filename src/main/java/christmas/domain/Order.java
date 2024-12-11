d38840c64fd5a8dbe8994d0ad7aa8cfcbdbfed71
minSsan <msp1125@hanyang.ac.kr>
2024-12-11 15:01:29 +0900

    feat(Order): 주문 정보 예외처리 추가

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -7,6 +7,7 @@ import java.util.EnumMap;
 import java.util.Map;
 
 public class Order {
+    public static final int MAX_COUNT = 20;
     private final Map<Menu, Integer> order = new EnumMap<>(Menu.class);
 
     public void addOrder(Menu menu, final int count) {
@@ -14,6 +15,13 @@ public class Order {
             throw new CustomException(ExceptionMessage.INVALID_ORDER.getMessage());
         }
         order.put(menu, count);
+        if (getTotalCount() > MAX_COUNT) {
+            throw new CustomException(ExceptionMessage.EXCEED_ORDER.getMessage());
+        }
+    }
+
+    private int getTotalCount() {
+        return order.values().stream().mapToInt(Integer::intValue).sum();
     }
 
     public int getMenuCount(MenuType target) {
