c3d3bc2280ad9ea0446b126c49e37c15c5ff4885
minSsan <msp1125@hanyang.ac.kr>
2024-12-11 17:53:04 +0900

    test(OrderTest): Order 클래스 테스트 케이스 추가

--- src/test/java/christmas/domain/OrderTest.java
+++ src/test/java/christmas/domain/OrderTest.java
@@ -1,15 +1,14 @@
 package christmas.domain;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import christmas.infrastructure.constants.ExceptionMessage;
-import christmas.infrastructure.exception.CustomException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class OrderTest {
@@ -35,5 +34,34 @@ class OrderTest {
         assertThatThrownBy(() -> {
             Order.of(input);
         }).describedAs(ExceptionMessage.INVALID_ORDER.getMessage());
+    }
+
+    @ParameterizedTest
+    @DisplayName("총 주문 금액을 올바르게 계산할 수 있다.")
+    @CsvSource(value = {"제로콜라-10, 초코케이크-1:45000", "양송이수프-1:6000"}, delimiter = ':')
+    void 총_주문_금액_계산(String input, int total) {
+        // given
+        Order order = Order.of(input);
+
+        // when
+        final int totalPrice = order.getTotalPrice();
+
+        // then
+        assertThat(totalPrice).isEqualTo(total);
+    }
+
+    @Test
+    @DisplayName("카테고리별 메뉴의 주문 수를 올바르게 반환할 수 있다.")
+    void 카테고리별_메뉴_개수_반환() {
+        // given
+        Order order = Order.of("티본스테이크-2, 크리스마스파스타-1, 제로콜라-1");
+
+        // when
+        final int mainCount = order.getMenuCount(MenuType.MAIN);
+        final int drinkCount = order.getMenuCount(MenuType.DRINK);
+
+        // then
+        assertThat(mainCount).isEqualTo(3);
+        assertThat(drinkCount).isEqualTo(1);
     }
 }
